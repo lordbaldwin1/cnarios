d91fc92aa1b6706a0ad0560e7fd9c8e16479da75
zach <springerzman@gmail.com>
2026-04-19 17:56:42 -0700

    button page e2e tests complete

--- pages/ButtonPage.ts
+++ pages/ButtonPage.ts
@@ -6,6 +6,7 @@ export class ButtonPage {
 
   readonly header: Locator;
   readonly tooltip: Locator;
+  readonly cardList: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -13,6 +14,7 @@ export class ButtonPage {
     this.enabledCards = this.page.locator(".MuiCard-root").filter({
       hasNot: this.page.getByRole("heading", { level: 6, name: "Unknown", exact: true })
     });
+    this.cardList = this.page.locator(".MuiCard-root");
     this.tooltip = this.page.getByRole("tooltip");
   }
 
@@ -23,12 +25,37 @@ export class ButtonPage {
   getEnabledCard() {
     const card = this.enabledCards.nth(0);
     const button = card.locator(".MuiCardContent-root").getByRole("button");
+    const removeButton = card.getByLabel("Remove");
     const icon = button.locator("svg");
     return {
       button,
       icon,
       clickFollow: async () => await button.click(),
       hoverFollow: async () => await button.hover(),
+      clickRemove: async () => await removeButton.click(),
     }
+  }
+
+  getCardByName(name: string) {
+    return this.cardList.filter({
+      hasText: name,
+    });
+  }
+
+  async getSuggestionNames() {
+    const res: string[] = [];
+    for (const card of (await this.cardList.all())) {
+      const title = await card.getByRole("heading").textContent();
+      if (!title) {
+        throw new Error("Test data error: card missing person name");
+      }
+      res.push(title);
+    }
+    return res;
+  }
+
+  async removeSuggestion(name: string) {
+    const cardToRemove = this.getCardByName(name);
+    await cardToRemove.getByLabel("Remove").click();
   }
 }
--- tests/button-page.spec.ts
+++ tests/button-page.spec.ts
@@ -1,9 +1,32 @@
 import { test as base, expect } from "@playwright/test";
 import { ButtonPage } from "../pages/ButtonPage";
 
+type bpTestData = {
+  startingSuggestions: string[];
+  expectedSuggestions: string[];
+};
+
 type Fixture = {
   bp: ButtonPage;
+  testData: bpTestData;
 };
+
+const testData: bpTestData = {
+  startingSuggestions: [
+    "Unknown",
+    "Anjali Sharma",
+    "Ravi Kumar",
+    "Neha Verma",
+    "Ram",
+  ],
+  expectedSuggestions: [
+    "Unknown",
+    "Ravi Kumar",
+    "Neha Verma",
+    "Ram",
+  ],
+};
+
 
 const test = base.extend<Fixture>({
   bp: async ({ page }, use) => {
@@ -11,6 +34,9 @@ const test = base.extend<Fixture>({
     await bp.goto();
     await use(bp);
   },
+  testData: async ({}, use) => {
+    await use(testData);
+  }
 });
 
 test.describe("Button Page tests", () => {
@@ -41,8 +67,24 @@ test.describe("Button Page tests", () => {
 
     await Promise.all([
       card.clickFollow(),
-      expect(card.button).toHaveText("Processing.."),
+      expect(card.button).toHaveText("Processing..."),
     ])
     await expect(card.button).toHaveText("Following");
-  })
+  });
+
+  test("BTN_004 Click Unfollow (toggle back)", async ({ bp }) => {
+    const card = bp.getEnabledCard();
+
+    await expect(card.button).toHaveText("Follow");
+    await card.clickFollow();
+    await expect(card.button).toHaveText("Following");
+    await card.clickFollow();
+    await expect(card.button).toHaveText("Follow");
+  });
+
+  test("BTN_005 Remove a suggestion card", async ({ bp, testData }) => {
+    expect(await bp.getSuggestionNames()).toEqual(testData.startingSuggestions);
+    await bp.removeSuggestion("Anjali Sharma");
+    expect(await bp.getSuggestionNames()).toEqual(testData.expectedSuggestions);
+  });
 });
