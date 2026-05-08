c719c5a616ef3b17c5b4aacabd03c1ab5f99868e
Zachary Springer <zach@mac.local>
2026-05-08 16:27:00 -0700

    add product listing tests

--- pages/ProductListingPage.ts
+++ pages/ProductListingPage.ts
@@ -9,21 +9,30 @@ export class ProductListingPage {
   readonly cards: Locator;
   readonly nextButton: Locator;
   readonly headerCounts: Locator;
+  readonly navigationButtons: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.cards = this.page.locator(".MuiCardContent-root");
     this.headerCounts = this.page.locator(".bg-white.border.rounded-lg");
-    this.nextButton = this.page.getByRole("button", { name: "Next", exact: true });
+    this.nextButton = this.page.getByRole("button", {
+      name: "Next",
+      exact: true,
+    });
+    this.navigationButtons = this.page
+      .getByRole("navigation", { name: "pagination navigation" })
+      .locator("li > button");
   }
 
   async goto() {
-    await this.page.goto("challenges/product-listing-pagination#challenge")
+    await this.page.goto("challenges/product-listing-pagination#challenge");
   }
 
   async getCardCategoryCount(count: CategoryCount) {
-    for (const card of (await this.cards.all())) {
-      const category = (await card.locator("p").innerText()).split(" ")[1].toUpperCase();
+    for (const card of await this.cards.all()) {
+      const category = (await card.locator("p").innerText())
+        .split(" ")[1]
+        .toUpperCase();
       count[category] = (count[category] || 0) + 1;
     }
   }
@@ -41,11 +50,44 @@ export class ProductListingPage {
   }
 
   async collectCategoryBannerCount(count: CategoryCount) {
-    for (const card of (await this.headerCounts.all())) {
+    for (const card of await this.headerCounts.all()) {
       const ptags = card.locator("p");
       const category = await ptags.nth(0).innerText();
       const cardCount = await ptags.nth(1).innerText();
       count[category] = Number.parseInt(cardCount);
     }
+  }
+
+  async findProduct(productName: string) {
+    let card: {
+      title: string;
+      category: string;
+      price: string;
+    } | null = null;
+    while (await this.nextButton.isEnabled()) {
+      card = await this.searchCards(productName);
+      if (card) {
+        return card;
+      }
+      await this.goNextPage();
+    }
+    card = await this.searchCards(productName);
+    return card;
+  }
+
+  async searchCards(productName: string) {
+    for (const card of await this.cards.all()) {
+      const headers = card.getByRole("heading", { level: 6 });
+      const title = await headers.nth(0).innerText();
+
+      if (title === productName) {
+        return {
+          title,
+          category: await card.getByRole("paragraph").innerText(),
+          price: await headers.nth(1).innerText(),
+        };
+      }
+    }
+    return null;
   }
 }
--- tests/product-listing.spec.ts
+++ tests/product-listing.spec.ts
@@ -1,24 +1,26 @@
 import { expect, test } from "../fixtures";
 import { CategoryCount } from "../pages/ProductListingPage";
 
-
 test.describe("ecommerce product listing & pagination tests", () => {
-// Test Steps & Details
-// Description:
-// Verify total number of products in each category matches expected values
-
-// Steps to Execute:
-// Navigate to product listing page
-// Iterate through all pages
-// Extract category for each product
-// Count products per category
-// Compare counts with expected values from product data file
-  test("product counts match category data source values", async ({ productListingPage }) => {
-    const count: CategoryCount = {};
-    const data: CategoryCount = {};
+  test("product counts match category data source values", async ({
+    productListingPage,
+  }) => {
+    const actualCount: CategoryCount = {};
+    const expectedCount: CategoryCount = {};
     await productListingPage.goto();
-    await productListingPage.collectAllCardCategoryCount(count);
-    await productListingPage.collectCategoryBannerCount(data);
-    expect(count).toEqual(data);
+    await productListingPage.collectAllCardCategoryCount(actualCount);
+    await productListingPage.collectCategoryBannerCount(expectedCount);
+    expect(actualCount).toEqual(expectedCount);
+  });
+
+  test("Uniqlo Ultra Light Down Jacket on page 3", async ({
+    productListingPage,
+  }) => {
+    await productListingPage.goto();
+    const card = await productListingPage.findProduct("Uniqlo Ultra Light Down Jacket");
+    if (!card) {
+      throw new Error("Product not found");
+    }
+    await expect(productListingPage.navigationButtons.nth(3)).toHaveAttribute("aria-current", "page");
   });
 });
